1a07791412bc090d13e2fa246a3a83bde3cc5029
GuihCFerreira <guihhh231@gmail.com>
2024-03-22 20:24:12 -0300

    (22/03/2024) Criação do projeto de back-end

---
+++ back-end/src/controller/user.js
@@ -0,0 +1,17 @@
+import prisma from '../database/client.js'
+
+const controller = {}
+
+controller.create = async function(req, res){
+    try {
+
+        await prisma.user.create({data: req.body})
+        res.status(201).end()
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).end()
+    }
+}
+
+export default controller
