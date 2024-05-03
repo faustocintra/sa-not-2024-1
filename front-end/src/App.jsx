643088abdcecfc5e4936e62dd94236698a6d7558
Rafael Chioca <rafaelmchioca@gmail.com>
2024-05-03 20:31:50 -0300

    (03/05) Segurança de menus no front-end

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,15 +1,21 @@
 import './App.css'
+import React from "react";
 import { BrowserRouter } from 'react-router-dom'
 import AppHeader from './ui/AppHeader'
 import AppRoutes from './routes/AppRoutes'
+import AuthUserContext from './contexts/AuthUserContext'
 
 function App() {
+  const [authUser, setAuthUser] = React.useState(null)
+
   return (
     <>
       <BrowserRouter>
-        <AppHeader />
-        <hr />
-        <AppRoutes />
+        <AuthUserContext.Provider value={{authUser, setAuthUser}}>
+          <AppHeader />
+          <hr />
+          <AppRoutes />
+        </AuthUserContext.Provider>
       </BrowserRouter>
     </>
   )
