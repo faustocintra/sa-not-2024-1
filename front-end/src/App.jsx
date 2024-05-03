bd2d1ea06fbb408c364385e6eb187242977a2caf
LUCAS GEAN DOS SANTOS <1091392313007@fatec-srv.local>
2024-05-03 20:27:58 -0300

    (03/05) Segurança de menus no front-end

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,15 +1,20 @@
+import React from 'react'
 import './App.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, useLocation } from 'react-router-dom'
 import AppHeader from './ui/AppHeader'
 import AppRoutes from './routes/AppRoutes'
+import AuthUserContext from './contexts/AuthUserContext'
 
 function App() {
+  const [authUser, setAuthUser] = React.useState(null)
   return (
     <>
       <BrowserRouter>
-        <AppHeader />
-        <hr />
-        <AppRoutes />
+        <AuthUserContext.Provider value={{ authUser, setAuthUser }}>
+          <AppHeader />
+          <hr />
+          <AppRoutes />
+        </AuthUserContext.Provider>
       </BrowserRouter>
     </>
   )
--- front-end/src/pages/UserList.jsx
+++ front-end/src/pages/UserList.jsx
@@ -15,7 +15,7 @@ export default function UserList() {
       const result = await myfetch.get('/users')
       setUsers(result)
     }
-    catch(error) {
+    catch (error) {
       console.log(error)
       alert(error.message)
     }
@@ -26,24 +26,26 @@ export default function UserList() {
       <h1>Listagem de usuários</h1>
 
       <table>
-        <tr>
-          <th>Cód.</th>
-          <th>Nome completo</th>
-          <th>Nome de usuário</th>
-          <th>É admin?</th>
-          <th>Editar</th>
-        </tr>
-        {
-          users.map(u => (
-            <tr>
-              <td>{u.id}</td>
-              <td>{u.fullname}</td>
-              <td>{u.username}</td>
-              <td>{u.is_admin ? 'Sim' : 'Não'}</td>
-              <td><Link to={`./${u.id}`}>[Editar]</Link></td>
-            </tr>
-          ))
-        }
+        <tbody>
+          <tr>
+            <th>Cód.</th>
+            <th>Nome completo</th>
+            <th>Nome de usuário</th>
+            <th>É admin?</th>
+            <th>Editar</th>
+          </tr>
+          {
+            users.map(u => (
+              <tr key={u.id}>
+                <td>{u.id}</td>
+                <td>{u.fullname}</td>
+                <td>{u.username}</td>
+                <td>{u.is_admin ? 'Sim' : 'Não'}</td>
+                <td><Link to={`./${u.id}`}>[Editar]</Link></td>
+              </tr>
+            ))
+          }
+        </tbody>
       </table>
     </>
   )
