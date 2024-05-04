04228ca53255cd8b9eaa32bbab57dd9663a82eb4
gabriel.carrijo <gabriel.carrijo@perinity.com>
2024-05-04 12:48:30 -0300

    criando ataque de força bruta

--- front-end/src/routes/AppRoutes.jsx
+++ front-end/src/routes/AppRoutes.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import AuthGuard from './AuthGuard'
 
 import HomePage from '../pages/HomePage'
 import LoginPage from '../pages/LoginPage'
 import UserList from '../pages/UserList'
 import UserForm from '../pages/UserForm'
 
+//Rotas protegidas por AuthGuard
+const guardedUserList = (
+  <AuthGuard userLevel={2}>
+    <UserList />
+  </AuthGuard>
+)
+const guardedUserForm = (
+  <AuthGuard userLevel={2}>
+    <UserForm />
+  </AuthGuard>
+)
+
 export default function AppRoutes() {
   return <Routes>
     <Route path="/" element={ <HomePage /> } />
     <Route path="/login" element={ <LoginPage /> } />
-    <Route path="/users" element={ <UserList /> } />
-    <Route path="/users/:id" element={ <UserForm /> } />
+    <Route path="/users" element={ guardedUserList } />
+    <Route path="/users/:id" element={guardedUserForm} />
   </Routes>
 }
