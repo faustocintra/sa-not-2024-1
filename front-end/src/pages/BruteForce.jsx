f76fcd5989a9ed80b6fcec6ac7ef2c56e3ccc0d0
Breno Tanaka <brenotanaka3@gmail.com>
2024-05-14 18:20:01 -0300

    (10/05) Término da lógica básica de bloqueio de tentativas de login

--- front-end/src/pages/BruteForce.jsx
+++ front-end/src/pages/BruteForce.jsx
@@ -2,58 +2,56 @@ import React from 'react';
 import wordlist from '../data/wordlist';
 import myfetch from '../lib/myfetch';
 
+let stop = false;
+
 export default function BruteForce() {
 
   const [log, setLog] = React.useState([]);
-  const [stop, setStop] = React.useState(false);
-  const [processing, setProcessing] = React.useState(false);
 
-  async function handleStartClick() {
-    setProcessing(true);
+  async function tryPassword(password) {
+    try {
+      await myfetch.post('/users/login', { username: 'admin', password });
+      return 'OK';
+    } catch (error) {
+      return error.message;
+    }
+  }
+
+  async function handleStartClick(event) {
+    event.target.disabled = true;
+    stop = false;
+
     for (let i=0; i<wordlist.length; i++) {
       if (stop) {
-        setProcessing(false);
         break;
       }
-      try {
-        const logCopy = [ ...log ];
-        logCopy.unshift({
-          num: i,
-          text: `Tentativa nº ${i} => ${wordlist[i]}`
-        });
-        setLog(logCopy);
-
-        await myfetch.post('/users/login', {
-          username: 'admin',
-          password: wordlist[i]
-        });
-        alert('SENHA ENCONTRADA: ' + wordlist[i]);
+      let result = await tryPassword(wordlist[i]);
+      if (result === 'OK') {
+        setLog(`SENHA ENCONTRADA, tentativa nº ${ i }: ${ wordlist[i] }`);
+        stop = true;
         break;
-      } catch(error) {
-        // Não faz nada
+      } else {
+        setLog(`Tentativa nº ${ i } (${ wordlist[i] }) => ${ result }`);
       }
+      setTimeout(() => setLog('-- parado --'), 250)
     }
-    setProcessing(false);
+    event.target.disabled = false;
   }
 
   return (
     <>
       <h1>Ataque de força bruta no <em>login</em></h1>
       <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-        <button onClick={ handleStartClick }  disabled={ processing }>
+        <button onClick={ handleStartClick }>
           Iniciar
         </button>
-        <button onClick={ () => setStop(true) } disabled={ !processing }>
+        <button onClick={ () => stop = true }>
           Parar
         </button>
       </div>
-      <select size="8">
-        {
-          log.map(row => (
-            <option key={ row.num } value={ row.text }>{ row.text }</option>
-          ))
-        }
-      </select>
+      <div style={{ fontFamily: 'monospace' }}>
+        { log }
+      </div>
     </>
   );
 }
