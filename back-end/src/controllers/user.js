85b98695e3e5e979f981a3f0189efad7e0f3cf75
ANA BEATRIZ RAMOS DOS SANTOS <1091392213001@fatec-srv.local>
2024-05-17 20:35:11 -0300

    (17/05) Armazenamento de token em cookie seguro

--- back-end/src/controllers/user.js
+++ back-end/src/controllers/user.js
@@ -1,6 +1,7 @@
 import prisma from '../database/client.js'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import { format, addMinutes } from 'date-fns'
 
 const controller = {}   // Objeto vazio
 
@@ -120,6 +121,60 @@ controller.delete = async function(req, res) {
   }
 }
 
+/*
+  Função que obtém ou determina os parâmetros necessários para validar
+  o login do usuário em função do número de tentativas e do tempo de 
+  atraso após o esgotamento do número de tentativas permitidas
+*/
+function getUserLoginParams(user) {
+  // Recuperamos os níveis de atraso da variável de ambiente
+  const delayLevels = process.env.DELAY_LEVELS.split(',')
+  
+  // Determinamos o nível de atraso atual e o próximo nível 
+  let currentDelayLevel, nextDelayLevel
+  if(user.delay_level < delayLevels.length - 1) {
+    currentDelayLevel = user.delay_level
+    nextDelayLevel = currentDelayLevel + 1
+  }
+  else if(user.delay_level === delayLevels.length) {
+    currentDelayLevel = user.delay_level
+    nextDelayLevel = currentDelayLevel
+  }
+  else {
+    currentDelayLevel = delayLevels.length - 1
+    nextDelayLevel = delayLevels.length - 1
+  }
+
+  // Determinamos o números minutos de atraso do nível atual
+  // e do próximo nível
+  const currentDelayMinutes = Number(delayLevels[currentDelayLevel])
+  const nextDelayMinutes = Number(delayLevels[nextDelayLevel])
+  
+  // Determinamos o momento do último login e a data/hora após as quais
+  // o usuário poderá tentar fazer login novamente
+  const lastLogin = user.last_attempt ? user.last_attempt : null
+  const canLoginAfter = lastLogin ? addMinutes(lastLogin, currentDelayMinutes) : new Date(1900, 1, 1)
+
+  // Determinamos o número de tentativas atuais e o próximo número de tentativas
+  // Sendo atingido o máximo de tentativas, a contagem reinicia em 0
+  const currentLoginAttempts = user.login_attempts
+  const nextLoginAttempts = 
+    currentLoginAttempts < Number(process.env.MAX_LOGIN_ATTEMPTS) ?
+    currentLoginAttempts + 1 :
+    0
+
+  return {
+    currentDelayLevel, 
+    nextDelayLevel,
+    currentDelayMinutes,
+    nextDelayMinutes,
+    lastLogin,
+    canLoginAfter,
+    currentLoginAttempts,
+    nextLoginAttempts
+  }
+}
+
 controller.login = async function(req, res) {
   try {
     // Busca o usuário pelo username
@@ -131,14 +186,70 @@ controller.login = async function(req, res) {
     // HTTP 401: Unauthorized
     if(! user) return res.status(401).end()
 
+    // Busca os parâmetros que serão usados na validação de tentativas
+    // e intervalo de login
+    const {
+      nextDelayLevel,
+      currentDelayMinutes,
+      canLoginAfter,
+      currentLoginAttempts,
+      nextLoginAttempts
+    } = getUserLoginParams(user)
+
+    console.log(`Tentativas: ${currentLoginAttempts}, pode tentar após ${format(canLoginAfter, "PPpp")}`)
+
+    // Usuário encontrado, precisamos verificar se ele ainda tem
+    // tentativas de login disponíveis ou se o tempo de atraso já expirou
+    if(user.login_attempts >= Number(process.env.MAX_LOGIN_ATTEMPTS) &&
+      new Date() < canLoginAfter) {
+      // Avisa que o usuário poderá tentar de novo após XXX milissegundos
+      res.setHeader('Retry-After', currentDelayMinutes * 60 * 1000)
+      // HTTP 429: Too Many Attempts
+      return res.status(429).end()
+    }
+
     // Usuário encontrado, vamos conferir a senha
     const passwordMatches = await bcrypt.compare(req.body.password, user.password)
 
-    // Se a senha estiver incorreta ~>
-    // HTTP 401: Unauthorized
-    if(! passwordMatches) return res.status(401).end()
+    // Se a senha estiver incorreta
+    if(! passwordMatches) {
+      // Incrementa o número de tentativas do usuário
+      if(currentLoginAttempts < Number(process.env.MAX_LOGIN_ATTEMPTS) - 1) {
+        await prisma.user.update({
+          where: { username: req.body.username.toLowerCase() },
+          data: { 
+            login_attempts: nextLoginAttempts,
+            last_attempt: new Date()   // Hora atual
+          }
+        })
+      }
+      else {
+        // Igualou o número máximo de tentativas, sobe o nível
+        // de espera para novas tentativas
+        await prisma.user.update({
+          where: { username: req.body.username.toLowerCase() },
+          data: { 
+            login_attempts: nextLoginAttempts,
+            delay_level: nextDelayLevel,
+            last_attempt: new Date()   // Hora atual
+          }
+        })
+      }
+      // HTTP 401: Unauthorized
+      return res.status(401).end()
+    }
 
     // Se chegamos até aqui, username + password estão OK
+    // Resetamos o número de tentativas e o nível de espera
+    await prisma.user.update({
+      where: { username: req.body.username.toLowerCase() },
+      data: { 
+        login_attempts: 0,
+        delay_level: 0,
+        last_attempt: new Date()   // Hora atual
+      }
+    })
+
     // Vamos criar o token e retorná-lo como resposta
 
     // O token inclui as informações do usuário. Vamos excluir o campo
@@ -151,8 +262,21 @@ controller.login = async function(req, res) {
       { expiresIn: '24h' }  // Prazo de validade do token
     )
 
+    // Formamos o cookie para enviar ao front-end
+    res.cookie(process.env.AUTH_COOKIE_NAME, token, {
+      httpOnly: true,   // O cookie ficará inacessível para JS
+      secure: true,
+      sameSite: 'Strict',
+      path: '/',
+      maxAge: 24 * 60 * 60 * 1000   // 24h em milissegundos
+    })
+
     // Retorna o token com status HTTP 200: OK (implícito)
-    res.send({token})
+    //res.send({token})
+
+    // O token não é mais enviado na resposta
+    // A resposta agora é simplesmente HTTP 204: No Content
+    res.status(204).end()
 
   }
   catch(error) {
@@ -162,9 +286,23 @@ controller.login = async function(req, res) {
   }
 }
 
+controller.logout = function(req, res) {
+  // Apaga o cookie que armazena o token de autorização
+  res.clearCookie(process.env.AUTH_COOKIE_NAME)
+  res.status(204).end()
+}
+
 controller.me = function(req, res) {
+
+  // Se houver usuário autenticado, ele foi salvo em req.authUser
+  // pelo middleware auth quando este conferiu o token. Portanto,
+  // para enviar informações do usuário logado ao front-end, basta
+  // responder com req.authUser
   if(req.authUser) res.send(req.authUser)
 
+  // Se req.authUser não existir, significa que não há usuário
+  // autenticado
+  // HTTP 401: Unauthorized
   else res.status(401).end()
 }
 
