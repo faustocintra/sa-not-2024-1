cca998dfaad79d33ef43c035ffa42c9c45454ec6
GuihCFerreira <guihhh231@gmail.com>
2024-05-24 20:24:10 -0300

    (24/05) início da validação do cadastro usuários

--- front-end/src/pages/UserForm.jsx
+++ front-end/src/pages/UserForm.jsx
@@ -1,87 +1,145 @@
-import React from "react";
-import {useNavigate, useParams} from 'react-router-dom'
-import myfetch from "../lib/myfetch";
+import React from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import myfetch from '../lib/myfetch'
 import './UserForm.css'
+import User from '../models/User'
+import { ZodError } from 'zod'
 
 export default function UserForm() {
+  const [state, setState] = React.useState({
+    user: {},
+    inputErrors: null
+  })
+  const {
+    user,
+    inputErrors
+  } = state
 
-    const [state, setState] = React.useState({ user: {}})
-    const {user} = state
+  const editPasswordRef = React.useRef()
 
-    const editPasswordRef = React.useRef()
+  const navigate = useNavigate()
+  const params = useParams()
 
-    const navigate = useNavigate();
-    const params = useParams();
+  React.useEffect(() => {
+    if(params.id) fetchData()
+  }, [])
 
-    React.useEffect(() => {
-        if(params.id) fetchData()
-    }, [])
-
-    async function fetchData() {
-        try {
-            const result = await myfetch.get(`/users/${params.id}`)
-            setState({...state, user: result})
-        } catch (error) {
-            alert('ERRO: ' + error.message)
-        }
+  async function fetchData() {
+    try {
+      const result = await myfetch.get(`/users/${params.id}`)
+      setState({ ...state, user: result })
     }
-
-    function handleFieldChange (e) {
-        const userCopy = {...user}
-        userCopy[e.target.value]= e.target.value
-        setState({...state, user: userCopy})
+    catch(error) {
+      alert('ERRO: ' + error.message)
     }
+  }
 
-    function handleEditPasswordToggle(e){
-        let style
-        if(e.target.checked) editPasswordRef.current.style.display= 'block'
-        else editPasswordRef.current.style.display='none'
+  function handleFieldChange(e) {
+    const userCopy = { ...user }
+    userCopy[e.target.name] = e.target.value
+    setState({ ...state, user: userCopy})
+  }
+
+  function handleEditPasswordToggle(e) {
+    if(e.target.checked) editPasswordRef.current.style.display = 'block'
+    else editPasswordRef.current.style.display = 'none'
+  }
+
+  function handleIsAdminClick(e) {
+    const userCopy = { ...user }
+    userCopy.is_admin = e.target.checked
+    setState({ ...state, user: userCopy })
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault()    // Impede o recarregamento da página
+    try {
+      // Invoca a validação do Zod por meio do model User
+      User.parse(user)
+
+      // Se a rota tiver o parâmetro id, significa que estamos editando
+      // um usuário
+      if(params.id) await myfetch.put(`/users/${params.id}`, user)
+
+      // Senão, estaremos criando um novo usuário
+      else await myfetch.post('/users', user)
+
+      alert('Dados salvos com sucesso.')
     }
+    catch(error) {
+      console.error(error)
 
-    function handleIsAdminClick (e){
-        const userCopy = {...user}
-        userCopy.is_admin = e.target.checked
-        setState({...state, user: userCopy})
+      // Verifica se há erros de validação do Zod
+      if(error instanceof ZodError) {
+        // Formamos um objeto contendo os erros do Zod e os colocamos
+        // na variável de estado inputErrors
+        const messages = {}
+        for(let i of error.issues) messages[i.path[0]] = i.message
+        setState({ ...state, inputErrors: messages })
+        alert('Há campos com valores inválidos no formulário. Verifique.')
+      }
+      else alert(error.message)
     }
+  }
 
-    return (
-        <>
-            <h1>{params.id ? `Editando usuário  ${params.id}`: 'Novo usuário'}</h1>
-            <form >
-                <div>
-                    <label>
-                        <span>Nome completo: </span>
-                        <input value={user.fullname} onChange={handleFieldChange}/>
-                    </label>
-                </div>
-                <div>
-                    <label>
-                        <span>Nome de usuário: </span>
-                        <input value={user.username} onChange={handleFieldChange}/>
-                    </label>
-                </div>
-                <div>
-                    <input type="checkbox" onClick={handleEditPasswordToggle} />
-                    &nbsp;<span>Alterar senha</span>
-                </div>
-                <div ref={editPasswordRef} className="edit-password">
-                    <label>
-                        <span>Digite a senha: </span>
-                        <input type="password" name="password" value={user.password} onChange={handleFieldChange}/>
-                    </label>
-                    <label>
-                        <span>Repita a senha: </span>
-                        <input type="password2" name="password" value={user.password2} onChange={handleFieldChange}/>
-                    </label>
-                </div>
-                <div>
-                    <input type="checkbox" onClick={handleIsAdminClick} />
-                    &nbsp;<span>Usuário é administrador</span>
-                </div>
-                <div>
-                    <button type="submit">Enviar</button>
-                </div>
-            </form>
-        </>
-    )
+  return (
+    <>
+      <h1>{ params.id ? `Editando usuário #${params.id}` : 'Novo usuário' }</h1>
+      <form onSubmit={handleSubmit}>
+
+        <div>
+          <label>
+            <span>Nome completo:</span>
+            <input name="fullname" value={user.fullname} onChange={handleFieldChange} />
+            <div className="input-error">
+              { inputErrors?.fullname }
+            </div>
+          </label>
+        </div>
+
+        <div>
+          <label>
+            <span>Nome de usuário:</span>
+            <input name="username" value={user.username} onChange={handleFieldChange} />
+            <div className="input-error">
+              { inputErrors?.username }
+            </div>
+          </label>
+        </div>
+
+        <div>
+          <input type="checkbox" onClick={handleEditPasswordToggle} />
+          &nbsp;<span>Alterar senha</span>
+        </div>
+
+        <div ref={editPasswordRef} className="edit-password">
+          <label>
+            <span>Digite a senha:</span>
+            <input name="password" type="password" value={user.password} onChange={handleFieldChange} />
+            <div className="input-error">
+              { inputErrors?.password }
+            </div>
+          </label>
+          
+          <label>
+            <span>Repita a senha:</span>
+            <input name="password2" type="password" value={user.password2} onChange={handleFieldChange} />
+            <div className="input-error">
+              { inputErrors?.password2 }
+            </div>
+          </label>
+        </div>
+
+        <div>
+          <input type="checkbox" checked={user.is_admin} onClick={handleIsAdminClick} />
+          &nbsp;<span>Usuário é administrador</span>
+        </div>
+
+        <div>
+          <button type="submit">Enviar</button>
+        </div>
+
+      </form>
+    </>
+  )
 }
