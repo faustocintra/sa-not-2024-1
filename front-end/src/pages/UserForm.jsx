1f52894575e785542a8c4abc23aadb7252c4134d
Cassio Silva Takarada <102415472+cassiotakarada@users.noreply.github.com>
2024-05-24 20:23:22 -0300

    (24/04) Início da validação do cadastro de usuários

--- front-end/src/pages/UserForm.jsx
+++ front-end/src/pages/UserForm.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import myfetch from "../lib/myfetch";
 import "./UserForm.css";
+import User from "../models/User";
+import { ZodError } from 'zod';
 
 export default function UserForm() {
   const [state, setState] = React.useState({
     user: {},
+    inputErrors: null
   });
-  const { user } = state;
+  const { user, inputErrors } = state;
 
   const editPasswordRef = React.useRef();
 
@@ -42,6 +45,34 @@ export default function UserForm() {
     const userCopy = { ...user };
     userCopy.is_admin = e.target.checkedset;
     setState({ ...state, user: userCopy });
+  }
+
+  async function handleSubmit (event) {
+    event.preventDefault()  // Impede o recarregamento da página
+    try {
+      // Invoca a validação do Zod por meio do model User
+      User.parse(user)
+
+      // Se a rota tiver o parâmetro id, siginifica que estamos editando um usuário
+      if(params.id) await myfetch.put(`/users/${params.id}`, user)
+
+      // Senão, estaremos criando um novo usuário
+      else await myfetch.post('/users', user)
+    }
+    catch(error) {
+      console.error(error)
+
+      // Verifica se há erros de validação do Zod
+      if(error instanceof ZodError) {
+        // Formamos um objeto contendo os erros do Zod e os colocamos na variável de estado
+        // inputErrors
+        const messages = {}
+        for (let i of error.issues) [i.path[0]] = i.message
+        setState({ ...state, inputErrors: messages })
+        alert('Há campos com valores inválidos no formulário. Verifique.')
+      }
+      else alert(error.message)
+    }
   }
 
   return (
