48b6104f9d475d845e2948ac2cc34dfcafb4fb4c
ANA BEATRIZ RAMOS DOS SANTOS <1091392213001@fatec-srv.local>
2024-05-25 12:23:39 -0300

    (25/05) FINALIZAÇÃO DO PROJETO

--- front-end/src/pages/LoginPage.jsx
+++ front-end/src/pages/LoginPage.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import myfetch from '../lib/myfetch'
 import { useNavigate } from 'react-router-dom'
-import {zodError} from 'zod'
+import { ZodError } from 'zod'
 import Login from '../models/Login'
+
 
 export default function LoginPage() {
   const [username, setUsername] = React.useState('')
@@ -16,15 +17,14 @@ export default function LoginPage() {
     try {
 
       // Invoca a validação do model Login
-      Login.parse({username, password})
-
+      Login.parse({ username, password })
 
       // Em aplicações de produção, aqui deveria ser mostrado
       // algum feedback de espera para o usuário
       //const result = await myfetch.post('/users/login', { username, password })
       await myfetch.post('/users/login', { username, password })
 
-      // Armazen a o token recebido no localStorage
+      // Armazena o token recebido no localStorage
       // ATENÇÃO: ABORDAGEM NÃO SEGURA, há outros meios
       // mais adequados de armazenar o token com segurança
       // window.localStorage.setItem(import.meta.env.VITE_AUTH_TOKEN_NAME, result.token)
@@ -34,19 +34,19 @@ export default function LoginPage() {
     }
     catch(error) {
       // Deu errado, permanecemos na página de login e 
-      // informamos o usuárioalert('Usuário ou senha inválidos.')
+      // informamos o usuário
       console.error(error)
 
-      if(error instanceof zodError) {
-        // Formamos um objeto contendo os erros do Zod e os colocamos 
+      if(error instanceof ZodError) {
+        // Formamos um objeto contendo os erros do Zod e os colocamos
         // na variável de estado inputErrors
-        const messsages = {}
-        for (let i of error.issues) messages[i.path[0]] = i.message
-        setInputErrors(messsages)
+        const messages = {}
+        for(let i of error.issues) messages[i.path[0]] = i.message
+        setInputErrors(messages)
         alert('Há campos com valores inválidos no formulário. Verifique.')
       }
-      if(error.status === 400) alert(error.message)
-      else alert('Usuario ou senha inválidos')
+      else if(error.status === 400) alert(error.message)
+      else alert('Usuário ou senha inválidos.')
     }
   }
 
@@ -59,7 +59,7 @@ export default function LoginPage() {
           <label>
             <span>Usuário:</span><br />
             <input value={username} onChange={e => setUsername(e.target.value)} />
-            <div className="input-errors">
+            <div className="input-error">
               { inputErrors?.username }
             </div>
           </label>
@@ -69,7 +69,7 @@ export default function LoginPage() {
           <label>
             <span>Senha:</span><br />
             <input value={password} type="password" onChange={e => setPassword(e.target.value)} />
-            <div className="input-errors">
+            <div className="input-error">
               { inputErrors?.password }
             </div>
           </label>
