b5b5b0c3e23879ba8d962dca61a1ef865a7b77da
Vanderlei Rocha <vanderleioleveira76@gmail.com>
2024-06-07 14:23:08 -0300

    (18/05) Validação com zod no back-end e no front-end

--- back-end/src/controllers/user.js
+++ back-end/src/controllers/user.js
@@ -2,6 +2,8 @@ import prisma from "../database/client.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { format, addMinutes } from "date-fns";
+import { ZodError } from "zod";
+import Login from "../models/Login.js";
 
 const controller = {}; // Objeto vazio
 
@@ -173,6 +175,9 @@ function getUserLoginParams(user) {
 
 controller.login = async function (req, res) {
   try {
+    // Invoca a validação dos campos definida no model Login
+    Login.parse(req.body);
+
     // Busca o usuário pelo username
     const user = await prisma.user.findUnique({
       where: { username: req.body.username.toLowerCase() },
@@ -281,8 +286,10 @@ controller.login = async function (req, res) {
     res.status(204).end();
   } catch (error) {
     console.error(error);
+    // HTTP 400: Bad Request
+    if (error instanceof ZodError) res.status(400).send(error.issues);
     // HTTP 500: Internal Server Error
-    res.status(500).send(error);
+    else res.status(500).send(error);
   }
 };
 
